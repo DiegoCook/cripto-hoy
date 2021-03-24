cf7c5939fa804cdd53f8befeba1cd47c6c15e097
Diego Cucaresse <diegocuca@Mac-mini-de-Diego.local>
2021-03-24 04:06:19 -0300

    Vue-router installed. View components created and imported

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas
+      importantes a traves de la API REST de Coincap.
+      <br />Creado por Diego Cucaresse.
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline">
+      Volver a la página de inicio
+    </router-link>
+  </div>
+</template>
